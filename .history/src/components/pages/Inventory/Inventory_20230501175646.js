ad44bba4ef67c68c82071b0edf030b415d5bc7eb
React1 <React1@flyweis.technology>
2023-05-01 18:06:54 +0530

    changes

---
+++ .history/src/components/pages/Inventory/Inventory_20230501175646.js
@@ -0,0 +1,286 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import HOC from "../../layout/HOC";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import { Button, Form, Modal } from "react-bootstrap";
+import { toast } from "react-toastify";
+
+const Inventory = () => {
+  const [data, setData] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+  const [query, setQuery] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/items"
+      );
+      setData(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function AddCrop(props) {
+    const [image, setImage] = useState("");
+    const [name, setName] = useState("");
+    const [grade, setGrade] = useState("");
+    const [variaty, setVariety] = useState("");
+    const [moisture, setMoisture] = useState("");
+    const [extraneous, setExtraneous] = useState("");
+    const [foreign, setForign] = useState("");
+    const [ gradeArray ,  setGradeArray ] = useState([])
+    const [ varietyArray ,  setGeArray ] = useState([])
+
+    const uploadImage = (e) => {
+      const data = new FormData();
+      data.append("file", e.target.files[0]);
+      data.append("upload_preset", "ml_default");
+      data.append("cloud_name", "dbcnha741");
+      fetch("https://api.cloudinary.com/v1_1/dbcnha741/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setImage(data.url);
+          console.log(data.url);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    function handleAddButtonClick() {
+      setGradeArray(prevArray => [...prevArray, grade]);
+      setGrade("")
+      toast.success("Grade Added")
+    }
+
+    const postHandler = async (e) => {
+      e.preventDefault();
+      try {
+        const { data } = await axios.post(
+          "https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/items",
+          {
+            image,
+            name,
+            grade : gradeArray,
+            variaty,
+            moisture,
+            extraneous,
+            foreign,
+          }
+        );
+        console.log(data);
+        toast.success("Added");
+        fetchData();
+        props.onHide();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Add Crop</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={postHandler}>
+            <Form.Group className="mb-3">
+              <Form.Label>Image</Form.Label>
+              <Form.Control type="file" onChange={(e) => uploadImage(e)} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+              <Form.Group className="mb-3">
+                <Form.Label>Grade</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={grade}
+                  onChange={(e) => setGrade(e.target.value)}
+                />
+              </Form.Group>
+             <img src='https://www.pngarts.com/files/3/Plus-Symbol-PNG-Image-with-Transparent-Background.png' alt='' style={{width : '30px' , cursor : 'pointer'}}  onClick= {() => handleAddButtonClick()} />
+            </div>
+
+            <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+              <Form.Group className="mb-3">
+                <Form.Label>Variety</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={(e) => setVariety(e.target.value)}
+                />
+              </Form.Group>
+              <img src='https://www.pngarts.com/files/3/Plus-Symbol-PNG-Image-with-Transparent-Background.png' alt='' style={{width : '30px' , cursor : 'pointer'}} />
+            </div>
+
+
+            <Form.Group className="mb-3">
+              <Form.Label>Moisture</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setMoisture(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Extraneous Matter </Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setExtraneous(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Foreign Matter</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setForign(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="outline-success" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    );
+  }
+
+  const deleteHandler = async (id) => {
+    try {
+      const { data } = await axios.delete(
+        `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/items/${id}`
+      );
+      console.log(data);
+      toast.success("Deleted");
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const filterData = !query
+    ? data?.result
+    : data?.result?.filter((i) =>
+        i?.name?.toLowerCase().includes(query?.toLowerCase())
+      );
+
+  return (
+    <>
+      <AddCrop show={modalShow} onHide={() => setModalShow(false)} />
+
+      <section>
+        <div className="pb-4 sticky top-0  w-full flex justify-between items-center bg-white">
+          <span className="tracking-widest text-slate-900 font-semibold uppercase ">
+            All Crops
+          </span>
+          <Button variant="outline-success" onClick={() => setModalShow(true)}>
+            Add Crop
+          </Button>
+        </div>
+      </section>
+
+      <div style={{ marginTop: "2%" }}>
+        <div style={{ color: "black" }}>
+          Search:{" "}
+          <input
+            type={"search"}
+            style={{
+              border: "1px solid #bfbfbf",
+              width: "250px",
+              color: "black",
+              padding: "5px",
+            }}
+            placeholder="Search by Name "
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div style={{ overflow: "auto" }}>
+        <Table
+          striped
+          bordered
+          hover
+          style={{
+            marginTop: "2%",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>SNo. </th>
+              <th> Crop Image </th>
+              <th> Crop Name </th>
+              <th> Variety </th>
+              <th> Grade </th>
+              <th> Moisture </th>
+              <th> Extraneous Matter </th>
+              <th>Foreign Matter</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterData?.map((i, index) => (
+              <tr key={index}>
+                <td>#{index + 1} </td>
+                <td>
+                  <img src={i.image} alt="" style={{ width: "100px" }} />
+                </td>
+                <td> {i.name} </td>
+                <td>
+                  {" "}
+                  {i.variaty?.map((i, index) => (
+                    <ul key={index} style={{ listStyleType: "disc" }}>
+                      <li> {i} </li>
+                    </ul>
+                  ))}{" "}
+                </td>
+                <td>
+                  {" "}
+                  {i.grade?.map((i, index) => (
+                    <ul key={index} style={{ listStyleType: "disc" }}>
+                      <li> {i} </li>
+                    </ul>
+                  ))}{" "}
+                </td>
+                <td> {i.moisture} </td>
+                <td> {i.extraneous} </td>
+                <td> {i.foreign} </td>
+                <td>
+                  <i
+                    class="fa-solid fa-delete-left"
+                    style={{ color: "red", cursor: "pointer" }}
+                    onClick={() => deleteHandler(i._id)}
+                  ></i>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
+};
+
+export default HOC(Inventory);
---
+++ .history/src/components/pages/products/Products_20230501171246.js
@@ -0,0 +1,414 @@
+/** @format */
+
+import React, { useState, useEffect, useCallback } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import HOC from "../../layout/HOC";
+import Table from "react-bootstrap/Table";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Products = () => {
+  const { role } = useParams();
+  const [modalShow, setModalShow] = React.useState(false);
+  const [id, setId] = useState("");
+  const [data, setData] = useState("");
+  const [secondTab, setSecondTab] = useState(false);
+  const [show, setShow] = useState(false);
+  const [lotIdData, setLotIdData] = useState([]);
+  const [query, setQuery] = useState("");
+  const [ modalShow2 , setModalShow2] = useState(false)
+
+  const fetchHandler = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/createbid/${role}`
+      );
+      setData(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [role]);
+
+  useEffect(() => {
+    fetchHandler();
+  }, [fetchHandler]);
+
+  const fetchSingleBid = async (lotId) => {
+    try {
+      const { data } = await axios.post(
+        `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev//createbid/lotId`,
+        {
+          lotId,
+        }
+      );
+      setLotIdData(data);
+      console.log(data);
+      setSecondTab(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  function MyVerticallyCenteredModal(props) {
+    const [expiretime, setExpireTime] = useState("");
+
+    const putHandler = async (e) => {
+      e.preventDefault();
+      try {
+        const { data } = await axios.put(
+          `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/createbid/verifyByAdmin/${id}`,
+          { expiretime }
+        );
+        console.log(data);
+        toast.success("Success");
+        props.onHide();
+        fetchHandler();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+ 
+
+    return (
+      <Modal
+        {...props}
+        size="lg-down"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Edit Status
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={putHandler}>
+            <Form.Group className="mb-3">
+              <Form.Label>Expiry Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setExpireTime(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="outline-success" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    );
+  }
+
+  // Top 10 Bidder
+  function BidModal(props) {
+    const [each, setEach] = useState([]);
+
+    const fetchBidder = useCallback(async () => {
+      try {
+        const { data } = await axios.post(
+          `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/createbid/waitlist/bid/${id}`
+        );
+        setEach(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }, []);
+
+    useEffect(() => {
+      if (props.show === true) {
+        fetchBidder();
+      }
+    }, [fetchBidder, props.show]);
+
+    const postHandler = async (status) => {
+      try {
+        const { data } = await axios.post(
+          `https://djqtflksic.execute-api.ap-south-1.amazonaws.com/dev/createbid/payment/status/${id}`,
+          {
+            status,
+          }
+        );
+        console.log(data);
+        fetchBidder();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    return (
+      <Modal
+        {...props}
+        size="lg-down"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Top 10 Bidder
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>SNo.</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {each?.message?.[0]?.top?.map((i, index) => (
+                <tr key={index}>
+                  <td> #{index + 1} </td>
+                  <td> {i.user?.tradeName} </td>
+                  <td> {i.status} </td>
+                  <td>
+                    <span className="d-flex gap-2">
+                      <i
+                        className="fa-solid fa-circle-xmark"
+                        style={{ color: "red", cursor: "pointer" }}
+                        onClick={() => {
+                          postHandler("decline");
+                        }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-circle-check"
+                        style={{ color: "green", cursor: "pointer" }}
+                        onClick={() => {
+                          postHandler("accept");
+                        }}
+                      ></i>
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    );
+  }
+
+  const filterData = !query
+    ? data?.data
+    : data?.data?.filter(
+        (i) =>
+          i?.user_id?.tradeName?.toLowerCase().includes(query?.toLowerCase()) ||
+          i?.crop?.name?.toLowerCase().includes(query?.toLowerCase()) ||
+          i?.lotId?.toString()?.toLowerCase().includes(query?.toLowerCase())
+      );
+
+  
+      function MyVerticallyCenteredModal3(props) { 
+
+        const [ data , setData ] = useState([])
+
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Transaction
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+             <div style={{width : '100%' , overflow  : 'auto'}}>
+              <Table striped bordered hover >
+                <thead>
+                  <tr>
+                    <td>SNo.</td>
+                    <td>Buyer Name</td>
+                    <td>Buyer Phone Number</td>
+                    <td>Supplier Name</td>
+                    <td>Supplier Phone Number</td>
+                    <td>Amount</td>
+                    <td>Status</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>#1</td>
+                    <td> Babu </td>
+                    <td>9999415074  </td>
+                    <td> demo </td>
+                    <td>9911161444  </td>
+                    <td> 1000  </td>
+                    <td> Payment Successfull  </td>
+                  </tr>
+                </tbody>
+              </Table>
+             </div>
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+          </Modal>
+        );
+      }
+
+  return (
+    <>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <BidModal show={show} onHide={() => setShow(false)} />
+
+      <MyVerticallyCenteredModal3 show={modalShow2} onHide={() => setModalShow2(false)} />
+
+      <section>
+        <div className="pb-4 sticky top-0  w-full flex justify-between items-center bg-white">
+          <span className="tracking-widest text-slate-900 font-semibold uppercase ">
+            {data?.data?.[0]?.supplierData?.tradeName} Bids
+          </span>
+        </div>
+      </section>
+
+      {secondTab ? (
+        <>
+          <Button
+            onClick={() => setSecondTab(false)}
+            variant="outline-success"
+            style={{ marginBottom: "10px" }}
+          >
+            Back
+          </Button>
+          <div
+            style={{
+              overflow: "auto",
+            }}
+          >
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Lot Id</th>
+                  <th>Product Name</th>
+                  <th>Variety</th>
+                  <th>Grade</th>
+                  <th>Quantity</th>
+                  <th>Total Bags</th>
+                  <th> Moisture </th>
+                  <th> Ad Mixture </th>
+                  <th>Foreign Matter </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {lotIdData?.result?.[0]?.lotId} </td>
+                  <td> {lotIdData?.result?.[0]?.crop?.name} </td>
+                  <td> {lotIdData?.result?.[0]?.variety} </td>
+                  <td> {lotIdData?.result?.[0]?.grade} </td>
+                  <td> {lotIdData?.result?.[0]?.quantity} </td>
+                  <td> {lotIdData?.result?.[0]?.totalBags} </td>
+                  <td> {lotIdData?.result?.[0]?.crop?.moisture} </td>
+                  <td> {lotIdData?.result?.[0]?.crop?.extraneous} </td>
+                  <td> {lotIdData?.result?.[0]?.crop?.foreign} </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </>
+      ) : (
+        <>
+          <div style={{ marginTop: "2%" }}>
+            <div style={{ color: "black" }}>
+              Search:{" "}
+              <input
+                type={"search"}
+                style={{
+                  border: "1px solid #bfbfbf",
+                  width: "250px",
+                  color: "black",
+                  padding: "5px",
+                }}
+                placeholder="Search by Name , Crop ..."
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div
+            style={{
+              overflow: "auto",
+              marginTop: "2%",
+            }}
+          >
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>SNo.</th>
+                  <th>Lot Id</th>
+                  <th>Supp Name</th>
+                  <th>Crop</th>
+                  <th>Status</th>
+                  <th>Expiry Time Edit</th>
+                  <th>Expected Bid</th>
+                  <th>Highest Bid</th>
+                  <th>Total Bid</th>
+                  <th>Top 10 Bidder/ Remaining Bidder</th>
+                  <th>Transaction</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filterData?.map((i, index) => (
+                  <tr key={index}>
+                    <td> #{index + 1} </td>
+                    <td
+                      onClick={() => {
+                        fetchSingleBid(i.lotId);
+                      }}
+                      style={{ color: "blue", cursor: "pointer" }}
+                    >
+                      {" "}
+                      {i.lotId}{" "}
+                    </td>
+                    <td> {i.user_id?.tradeName} </td>
+                    <td> {i.crop?.name} </td>
+                    <td> {i.status} </td>
+                    <td> {i.expiretime} </td>
+                    <td> {i.expectedRate} </td>
+                    <td> {i.topBid} </td>
+                    <td> {i.count} </td>
+                    <td>
+                      <Button
+                        onClick={() => {
+                          setId(i._id);
+                          setShow(true);
+                        }}
+                      >
+                        View
+                      </Button>
+                    </td>
+                    <td>
+                      <Button onClick={() => setModalShow2(true)} >View</Button>
+                    </td>
+                    <td>
+                      <i
+                        className="fa-solid fa-pen-to-square"
+                        style={{ color: "blue", cursor: "pointer" }}
+                        onClick={() => {
+                          setId(i._id);
+                          setModalShow(true);
+                        }}
+                      ></i>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HOC(Products);
